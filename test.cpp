84dbc1beceeeec073e6bef4fc5d231278a4d4f26
Mayank Gupta <ce.mayank8@gmail.com>
2022-03-13 05:52:05 -0700

    testing done

--- test.cpp
+++ test.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main(){
     
-    cout<<"Hello world"<<endl<<"Preferred style";
-    
+    cout<<"Hello world"<<endl<<"Preferred styles";
+    cout << "This is testing file";
+
     return 0;
 }
